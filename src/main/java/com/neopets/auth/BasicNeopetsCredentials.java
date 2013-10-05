75bb768d40d6b12435e7e8b8a54a2ca8646a3857
Peter Slawski <slawski.peter@gmail.com>
2013-10-05 00:47:44 -0400

    Add equals, hashCode, toString to credential implementations

--- src/main/java/com/neopets/auth/BasicNeopetsCredentials.java
+++ src/main/java/com/neopets/auth/BasicNeopetsCredentials.java
@@ -28,4 +28,31 @@ public class BasicNeopetsCredentials implements NeopetsCredentials {
     return password;
   }
 
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof BasicNeopetsCredentials)) return false;
+
+    BasicNeopetsCredentials that = (BasicNeopetsCredentials) o;
+
+    if (password != null ? !password.equals(that.password) : that.password != null) return false;
+    if (username != null ? !username.equals(that.username) : that.username != null) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = username != null ? username.hashCode() : 0;
+    result = 31 * result + (password != null ? password.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "BasicNeopetsCredentials{" +
+            "username='" + username + '\'' +
+            ", password='" + password + '\'' +
+            '}';
+  }
 }
--- src/main/java/com/neopets/auth/PropertiesNeopetsCredentials.java
+++ src/main/java/com/neopets/auth/PropertiesNeopetsCredentials.java
@@ -60,4 +60,32 @@ public class PropertiesNeopetsCredentials implements NeopetsCredentials {
   public String getPassword() {
     return password;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof PropertiesNeopetsCredentials)) return false;
+
+    PropertiesNeopetsCredentials that = (PropertiesNeopetsCredentials) o;
+
+    if (password != null ? !password.equals(that.password) : that.password != null) return false;
+    if (username != null ? !username.equals(that.username) : that.username != null) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = username != null ? username.hashCode() : 0;
+    result = 31 * result + (password != null ? password.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "PropertiesNeopetsCredentials{" +
+            "username='" + username + '\'' +
+            ", password='" + password + '\'' +
+            '}';
+  }
 }
